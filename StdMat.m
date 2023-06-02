3bad8f3e8076fe269798ce1d63c55cc874fed4d5
Tiger Sharks <77241431+Tiger-Sharks@users.noreply.github.com>
2023-06-02 11:07:31 +0530

    Standard Matrices
    Generating Standard Matrices.

---
+++ StdMat.m
@@ -0,0 +1,17 @@
+clc;
+clear all;
+close all;
+
+A = [1 0; 0 1]
+B = [0 1; 1 0]
+
+C = [A B]
+D = [A; B]
+E = eye(3)
+F = ones(4, 3)
+G = zeros(2, 3)
+H = rand(3, 4)
+I = round(H)
+
+J = [1 2 3; 5 6 7]
+K = reshape(J, 3, 2)
