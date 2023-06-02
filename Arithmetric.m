ecfb87ea884620d4ef754d514f44d08727afb1d3
Tiger Sharks <77241431+Tiger-Sharks@users.noreply.github.com>
2023-06-02 10:56:20 +0530

    Arithmetic Operations
    Performing Arithmetic Operations on Variables.

---
+++ Arithmetric.m
@@ -0,0 +1,19 @@
+clc;
+clear all;
+close all;
+
+a = 10
+b = 5
+
+c = a + b
+d = a - b
+e = a * b
+f = a / b
+g = a \ b
+h = a ^ b
+i = sqrt(a)
+j = log(b)
+k = exp(a)
+l = (pi/2)
+m = sin(pi / 2)
+n = cos(pi)
