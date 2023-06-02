e7a45d37efe680f82de13c6455fd1118399036ac
Tiger Sharks <77241431+Tiger-Sharks@users.noreply.github.com>
2023-06-02 11:25:37 +0530

    User-Defined Functions
    Calculating the Perimeter and Area of various shapes with the help of User-Defined Functions.

---
+++ ShapeArUser.m
@@ -0,0 +1,13 @@
+clc;
+clear all;
+close all;
+
+a = 2;
+b = 3;
+c = 4;
+d = 5;
+
+[sq_per1, sq_ar1, cir_per1, cir_ar1, sph_ar1] = func48a(a)
+[sq_per2, sq_ar2, cir_per2, cir_ar2, sph_ar2] = func48a(b)
+[sq_per3, sq_ar3, cir_per3, cir_ar3, sph_ar3] = func48a(c)
+[sq_per4, sq_ar4, cir_per4, cir_ar4, sph_ar4] = func48a(d)
---
+++ func48a.m
@@ -0,0 +1,7 @@
+function[sq1, sq2, cir1, cir2, sph] = func410(r)
+
+sq1 = 4 * r;
+sq2 = r * r;
+cir1 = 2 * pi * r;
+cir2 = pi * r * r;
+sph = 4 * pi * r * r;
